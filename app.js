1917ad6ba548a919c7b486cc1cff7508fc18e85a
sergeyd63 <sergeyd63@hotmail.com>
2018-07-27 19:25:13 -0400

    updated size of the file config

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let args = process.argv.slice(2);
 // http://028eed10.bwtest-aws.pravala.com/384MB.jar
 let url = args[0];
 let chunksParam = args[1] || 4;
-let chunkSize = parseInt(4194304 / chunksParam);
+let chunkSize = parseInt(4000000 / chunksParam);
 console.log(`Chunks: ${chunksParam}`);
 console.log(`Chunk size: ${chunkSize}`);
 
